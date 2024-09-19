8d6cfd746bb9b1da53054a3366319e602543e337
Yuval-Reyes <2350466a@umich.mx>
2024-09-19 12:41:12 -0600

    carga tarea verion 1.0
    En esta carga se sube la primera version de la tarea de interes simple

---
+++ formulasInteresSimple.R
@@ -0,0 +1,11 @@
+# se presentan la funciones de interes simple
+#autor: Dana Yuval Reyes González
+#v1.0: 19 septiembre de 2024
+
+#valor futuro con interes simple:
+
+valorfinalsimple=function(VA,r,t){
+xSalida=VA*(1+(r*t))  
+
+return(xSalida)
+}
